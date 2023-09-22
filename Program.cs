86223ea463cd23e0a105dfc3e6efed5923942d44
IgorMachad0 <igormaf019@gmail.com>
2023-09-22 01:59:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+using System;
+
+
+try
+{
+    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(
+        "User Id=sa;Password=1234;" +
+        "Database=testProject;" +
+        "Server=localhost\\SQLEXPRESS;" +        
+        "Trusted_Connection=False;"
+    );
+
+    using (SqlConnection Connect = new SqlConnection(builder.ConnectionString))
+    {
+
+        string sql = "Select * from customers";
+
+        using (SqlCommand Command = new SqlCommand(sql, Connect))
+        {
+
+            Connect.Open();
+            using (SqlDataReader Reader = Command.ExecuteReader())
+            {
+
+                while (Reader.Read())
+                {
+
+                    System.Console.WriteLine("id: {0} ", Reader[0]);
+                }
+            }
+        }
+    }
+
+}
+catch (System.Exception)
+{
+
+    throw;
+}
